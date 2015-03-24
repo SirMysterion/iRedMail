415cc2cb5a2867382bdf88ea97493655f6f2afaa
Dashamir Hoxha <dashohoxha@gmail.com>
2015-03-24 22:24:40 +0100

    Changed image names to lowercase.

--- docker-build.sh
+++ docker-build.sh
@@ -2,12 +2,12 @@
 
 ### build an initial image
 cd $(dirname $0)
-docker build -t iRedMail:init .
+docker build -t iredmail:init .
 
 ### run the config script (iRedMail.sh) on the initial image
-docker run -it --name=iRedMail-config iRedMail:init /$IREDMAIL/iRedMail.sh
+docker run -it --name=iredmail-config iredmail:init /$IREDMAIL/iRedMail.sh
 
 ### save the configured container as an image
-docker rmi iRedMail
-docker commit iRedMail-config iRedMail
-docker rm iRedMail-config
+docker rmi iredmail
+docker commit iredmail-config iredmail
+docker rm iredmail-config
