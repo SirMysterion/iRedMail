54ba3f5c66e907ae568b5656c1e35645c3f687aa
Dashamir Hoxha <dashohoxha@gmail.com>
2015-03-26 10:54:49 +0100

    Disable spam and virus checking, etc.

--- sysconfig.sh
+++ sysconfig.sh
@@ -11,3 +11,14 @@ sed -i /root/.bashrc \
 ### copy config files over to the system
 dir=$(dirname $0)
 cp -TdR $dir/sysconfig/ /
+
+### disable spam and virus checking (consumes too much resources)
+### see: http://www.iredmail.org/docs/completely.disable.amavisd.clamav.spamassassin.html
+sed -i /etc/amavis/conf.d/50-user \
+    -e '/^@bypass_virus_checks_maps/,+4 s/^/#/' \
+    -e '/^@bypass_spam_checks_maps/,+4 s/^/#/'
+
+### disable some other services
+update-rc.d sogo disable
+update-rc.d clamav-daemon disable
+update-rc.d clamav-freshclam disable
